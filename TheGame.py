63ac926806da9b19993042b742ccff3655646bf3
Balboran <Soeren.m93@outlook.de>
2020-06-09 00:46:28 +0200

    add some stuff

--- TheGame.py
+++ TheGame.py
@@ -1,9 +1,16 @@
+import time
+
 print("Hello World")
 
 felixstinks = True
-
+fs = 2
 while felixstinks:
   print("Felix müffelt")
-  
+  time.sleep(0.5)
+  fs = fs*2
+  print(fs)
+
+  if fs >= 100:
+    felixstinks = False
 else:
   print("Tut er trotzdem!")
