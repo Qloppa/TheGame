395f73c5e3fc421e8ab1a66270e5944dd5d401b7
Qloppa <felixrichter37@yahoo.de>
2020-06-13 16:53:00 +0200

    add class HandKarten

--- TheGame.py
+++ TheGame.py
@@ -12,7 +12,7 @@ class Spielkarte:
 class Kartenstapel:
 
   spielKarten = []
-  spK = np.arange(1,101, 1).tolist() #spK = Spielkarten // Erstellt eine Liste der Zahlen 1 - 100
+  spK = np.arange(1,11, 1).tolist() #spK = Spielkarten // Erstellt eine Liste der Zahlen 1 - 100
   #print(spK)
 
   for spK0 in spK:
@@ -25,7 +25,34 @@ class Kartenstapel:
   def __init__(self):
     print("Kartenstapel initialisiert")
 
+
+class HandKarten:
+
+  handKarten = []
+
+  def __init__(self):
+    print("HandKarten initialisiert")
+
+  def nimmHandKarten(self, anzahl):
+    for x in range(anzahl):
+      self.handKarten.append(Kartenstapel.spielKarten.pop())
+
 print("The Game:")
+
+print("Kartenstapel:")
+
+for spielKarte in Kartenstapel.spielKarten:
+  time.sleep(1.5)
+  print(spielKarte.getValue())
+
+print("Handkarten:")
+
+HandKarten = HandKarten()
+HandKarten.nimmHandKarten(7)
+
+for karte in HandKarten.handKarten:
+  time.sleep(1.5)
+  print(karte.getValue())
 
 print("Kartenstapel:")
 
